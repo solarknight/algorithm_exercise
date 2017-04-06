c5a4489b3c5e203b4a35db878ace3f247d7c877d
solarknight <zhouthanos@gmail.com>
2017-04-06 20:01:54 +0800

    change BinaryNumber case

--- algorithm/src/main/java/com/thanos/algorithm/exercise/BinaryNumber.java
+++ algorithm/src/main/java/com/thanos/algorithm/exercise/BinaryNumber.java
@@ -13,7 +13,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class BinaryNumber {
 
-  public static String toBinaryString(int input) {
+  public static String toBinaryString1(int input) {
     String s = "";
     for (int n = input; n > 0; n = n >> 1) {
       s = (n % 2) + s;
@@ -21,7 +21,15 @@ public class BinaryNumber {
     return s;
   }
 
-  public static String toBinaryStringOptimized(int input) {
+  public static String toBinaryString2(int input) {
+    StringBuilder sb = new StringBuilder(32);
+    for (int n = input; n > 0; n = n >> 1) {
+      sb.append(n % 2);
+    }
+    return sb.reverse().toString();
+  }
+
+  public static String toBinaryString3(int input) {
     StringBuilder sb = new StringBuilder(32);
     while (input != 0) {
       if ((input & 1) != 0) {
@@ -35,17 +43,16 @@ public class BinaryNumber {
     return sb.reverse().toString();
   }
 
-
   @Benchmark
   @BenchmarkMode(Mode.AverageTime)
   @Fork(1)
   @OutputTimeUnit(TimeUnit.MILLISECONDS)
-  @Warmup(iterations = 3)
+  @Warmup(iterations = 5)
   @Measurement(iterations = 5)
-  public void measureOrigin() {
+  public void measureMethod1() {
     int count = 1 << 20;
     for (int i = 0; i < count; i++) {
-      toBinaryString(1232437439);
+      toBinaryString1(1232437439);
     }
   }
 
@@ -53,12 +60,25 @@ public class BinaryNumber {
   @BenchmarkMode(Mode.AverageTime)
   @Fork(1)
   @OutputTimeUnit(TimeUnit.MILLISECONDS)
-  @Warmup(iterations = 3)
+  @Warmup(iterations = 5)
   @Measurement(iterations = 5)
-  public void measureOptimized() {
+  public void measureMethod2() {
     int count = 1 << 20;
     for (int i = 0; i < count; i++) {
-      toBinaryStringOptimized(1232437439);
+      toBinaryString2(1232437439);
+    }
+  }
+
+  @Benchmark
+  @BenchmarkMode(Mode.AverageTime)
+  @Fork(1)
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  @Warmup(iterations = 5)
+  @Measurement(iterations = 5)
+  public void measureMethod3() {
+    int count = 1 << 20;
+    for (int i = 0; i < count; i++) {
+      toBinaryString3(1232437439);
     }
   }
 }
