c96c266c569c013aa9f8bda647bb8cf07cf8bae4
solarknight <zhouthanos@gmail.com>
2017-04-11 01:46:24 +0800

    add TwoSortedArrayMedian java ver

--- leetcode/src/main/java/com/thanos/algorithm/jav/TwoSortedArrayMedian.java
+++ leetcode/src/main/java/com/thanos/algorithm/jav/TwoSortedArrayMedian.java
@@ -12,9 +12,36 @@ package com.thanos.algorithm.jav;
 public class TwoSortedArrayMedian {
 
   public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    int l1 = nums1.length, l2 = nums2.length;
+    if (l1 > l2) {
+      return findMedianSortedArrays(nums2, nums1);
+    }
 
+    if (l1 == 0) {
+      return (nums2[(l2 - 1) / 2] + nums2[l2 / 2]) / 2.0;
+    }
+
+    int left = 0, right = 2 * l1;
+    while (left <= right) {
+      int mid1 = (left + right) / 2;
+      int mid2 = l1 + l2 - mid1;
+
+      int lv1 = mid1 == 0 ? Integer.MIN_VALUE : nums1[(mid1 - 1) / 2];
+      int rv1 = mid1 == 2 * l1 ? Integer.MAX_VALUE : nums1[mid1 / 2];
+      int lv2 = mid2 == 0 ? Integer.MIN_VALUE : nums2[(mid2 - 1) / 2];
+      int rv2 = mid2 == 2 * l2 ? Integer.MAX_VALUE : nums2[mid2 / 2];
+
+      if (lv1 <= rv2 && lv2 <= rv1) {
+        return (Math.max(lv1, lv2) + Math.min(rv1, rv2)) / 2.0;
+      } else if (lv1 > rv2) {
+        right = mid1 - 1;
+      } else {
+        left = mid1 + 1;
+      }
+    }
     return -1;
   }
+
 
   public static void main(String[] args) {
     int[] array1, array2;
@@ -24,6 +51,10 @@ public class TwoSortedArrayMedian {
 
     array1 = new int[]{1, 2};
     array2 = new int[]{3, 4};
+    System.out.println(findMedianSortedArrays(array1, array2));
+
+    array1 = new int[]{6, 10};
+    array2 = new int[]{1, 2, 3, 4, 5, 7, 8, 9};
     System.out.println(findMedianSortedArrays(array1, array2));
   }
 }
