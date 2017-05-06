87e670b5837bb3f274fda32da687f177b48f3f4c
solarknight <zhouthanos@gmail.com>
2017-05-07 00:04:32 +0800

    optimization

--- leetcode/src/main/ruby/three_sum.rb
+++ leetcode/src/main/ruby/three_sum.rb
@@ -3,7 +3,7 @@
 def three_sum(nums)
     res = Array.new(0) {Array.new(0, 0)}
     return res if nums.length < 3
-    nums = nums.sort
+    nums.sort!
     for i in 0...nums.length - 2
         next if i != 0 && nums[i] == nums[i - 1]
         tar, j, k = -nums[i], i + 1, nums.length - 1
