bdc441100c9afa083a20d0f6cd26ab3bb5ed6ea0
solarknight <zhouthanos@gmail.com>
2017-05-13 22:43:29 +0800

    add GenerateParentheses.java

--- leetcode/src/main/java/GenerateParentheses.java
+++ leetcode/src/main/java/GenerateParentheses.java
@@ -11,8 +11,34 @@ public class GenerateParentheses {
     if (n == 0) {
       return res;
     }
-    // TODO: 2017/5/10
+    char[] c = new char[2 * n];
+    recursive(res, c, 0, n, 0, 0);
     return res;
   }
 
+  public void recursive(List<String> result, char[] c, int idx, int max, int open, int close) {
+    if (idx == c.length) {
+      result.add(new String(c));
+      return;
+    }
+    if (open < max) {
+      c[idx] = '(';
+      recursive(result, c, idx + 1, max, open + 1, close);
+    }
+    if (close < open) {
+      c[idx] = ')';
+      recursive(result, c, idx + 1, max, open, close + 1);
+    }
+  }
+
+  public static void main(String[] args) {
+    GenerateParentheses demo = new GenerateParentheses();
+    print(demo.generateParenthesis(2));
+  }
+
+  private static void print(List<String> list) {
+    for (String str : list) {
+      System.out.println(str);
+    }
+  }
 }
