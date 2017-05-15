9e9aa8dda8dcd3d12f8564ffcfac2fa96c2e479a
solarknight <zhouthanos@gmail.com>
2017-05-15 23:31:09 +0800

    update

---
+++ leetcode/src/main/java/SwapNodes.java
@@ -0,0 +1,24 @@
+/**
+ * Created by solarknight on 2017/5/15.
+ */
+public class SwapNodes {
+
+  public static class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+      val = x;
+    }
+  }
+
+  public ListNode swapPairs(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+    ListNode n = head.next;
+    head.next = swapPairs(head.next.next);
+    n.next = head;
+    return n;
+  }
+}
